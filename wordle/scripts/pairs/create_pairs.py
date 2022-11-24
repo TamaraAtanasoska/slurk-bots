e3c0d3acd12e64043a7b1eacf60b7165a6c2883a
Tamara Atanasoska <atanasoska.tamara@gmail.com>
2022-11-24 14:51:57 +0100

    WIP Generating data for image-word pairs

--- wordle/scripts/pairs/create_pairs.py
+++ wordle/scripts/pairs/create_pairs.py
@@ -2,12 +2,101 @@ import argparse
 import json
 import requests
 import pathlib
+import os
+import time
 
-from wordhoard import Synonyms, Hypernyms, Hyponyms
+from collections import defaultdict
+
+from wordhoard import Synonyms, Hypernyms
 
 
-def create_pairs(args):
-   pass 
+def get_syn_hyper(word: str):
+    """
+        A function to all the wordhoard related APIs and retrieve a given word's
+    synonyms and hypernyms"
+    """
+    synonym = Synonyms(search_string=word)
+    hypernym = Hypernyms(search_string=word)
+
+    synonyms = synonym.find_synonyms()
+    hypernyms = hypernym.find_hypernyms()
+
+    if any("No synonyms" in s for s in synonyms):
+        synonyms = []
+    if any("No hypernyms" in s for s in hypernyms):
+        hypernyms = []
+
+    return synonyms, hypernyms
+
+
+def get_related_terms(word: str):
+    """
+        This function calls the ConceptNet API and gets the 10 most related
+    terms and their weights.
+    """
+    obj = requests.get(
+        "https://api.conceptnet.io/related/c/en/" + word + "?filter=/c/en"
+    ).json()
+    return obj['related']
+
+
+def generate_file(sysnets_words: list):
+    """
+        This function takes the words associated with each sysnet and generates a
+    file with their synonyms, hypernims, hyponims and weighted related terms from
+    ConceptNet. Some APIs have a very low limit on calls, and a long wait time
+    is necessary, additionally because of a bug in the library."
+    """
+    term_list = []
+    print("Number of words to process: ", len(sysnets_words))
+    for num_word, word in enumerate(sysnets_words):
+        related_terms = get_related_terms(word[1])
+        synonyms, hypernyms = get_syn_hyper(word[1])
+        term_list.append(
+            [
+                word[0],
+                word[1],
+                synonyms,
+                hypernyms,
+                related_terms,
+            ]
+        )
+        time.sleep(3)
+        print("Processed word number ", num_word + 1, " : ", word[1])
+    print("All the words finished processing!")
+    print("File is available at: ")
+    return term_list
+
+
+def sysnet_to_word(path: pathlib.Path):
+    """
+        This function generates a list of available sysnets in the image directory
+    and their idenfifying words. It takes the path to the image directory and the
+    file that lists all available ImageNet words, then creates a joint list.
+    """
+    cur_dir = os.getcwd()
+
+    os.chdir(path)
+    sysnets = [name for name in os.listdir(".") if os.path.isdir(name)]
+
+    os.chdir(cur_dir)
+    lines = [x.split() for x in open("words.txt").readlines()]
+    sysnets_words = []
+    for line in lines:
+        for sysnet in sysnets:
+            if sysnet == line[0]:
+                sysnets_words.append([line[0], line[1].rstrip(",")])
+    return sysnets_words
+
+
+def create_pairs(args: argparse.Namespace):
+    """
+        The main function to create the pairs. Retrieves the sysnets, words and all
+    required related terms, then makes custom defined pairs according to the
+    parameters passed to the script.
+    """
+    sysnets_words = sysnet_to_word(args.path)
+    generate_file(sysnets_words)
 
 
 if __name__ == "__main__":
